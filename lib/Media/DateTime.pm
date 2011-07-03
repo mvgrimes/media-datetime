d2385008240f5bd8fbc96c48540a9a5866c60c1c
Mark Grimes <mgrimes@cpan.org>
2011-07-03 11:54:59 -0400

    Initial import from svn

---
+++ lib/Media/DateTime.pm
@@ -0,0 +1,136 @@
+package Media::DateTime;
+
+###########################################################################
+# Media::DateTime
+# Mark Grimes
+#
+# A simple module to extract the timestamp from media files in an flexible 
+# manner.
+#
+# Copyright (c) 2006 Mark Grimes (mgrimes@cpan.org).
+# All rights reserved. This program is free software; you can redistribute
+# it and/or modify it under the same terms as Perl itself.
+#
+# Formatted with tabstops at 4
+###########################################################################
+
+use strict;
+use warnings;
+
+use Carp;
+use DateTime;
+use Module::Pluggable 
+		search_path	=> 'Media::DateTime',
+		require		=> 1,		
+		sub_name	=> 'matchers';
+
+our $VERSION = '0.44';
+
+sub new {
+	my $that  = shift;
+	my $class = ref($that) || $that;	# Enables use to call $instance->new()
+	return bless {}, $class;
+}
+
+sub datetime {
+	my ($self,$f) = @_;
+	for my $class ($self->matchers){
+		if( $class->match( $f ) ){
+			my $v = $class->datetime( $f );
+			return $v if defined $v;
+		}
+	}
+	return $self->_datetime_from_filesystem_stamp( $f );
+}
+
+sub _datetime_from_filesystem_stamp {
+	my ($self, $f) = @_;
+
+	my $c_date = (stat($f))[9];
+	return DateTime->from_epoch( epoch => $c_date, time_zone => 'local' );
+}
+
+1;
+
+__END__
+
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Media::DateTime - An highly extensible module to extract the creation
+date and time from a file.
+
+=head1 SYNOPSIS
+
+  use DateTime;
+  use Media::DateTime;
+  my $dt = Media::DateTime->datetime( $file );
+  
+  # or more cleanly OO
+  my $dater = Media::DateTime->new;
+  my $dt = $dater->datetime( $file );
+
+=head1 DESCRIPTION
+
+Provides a very simple, but highly extensible method of extracting the
+creation date and time from a media file (any file really). The base
+module comes with support for JPEG files that store the creation date 
+in the exif header. 
+
+Plugins can be written to support any file format. See the 
+C<Media::DateTime::JPEG> module for an example.
+
+If no plugin is found for a particular file (or the plugin returns 
+a false vale) the file creation date as specified by the O/S is used.
+
+Returns a C<DateTime> object.
+
+=head1 METHODs
+
+=over 2
+
+=item new
+
+Constructor that returns a C<Media::DateTime> object. Methods can be
+called on either the class or an instance.
+
+	my $dt = Media::DateTime->new;
+
+=item datetime
+
+Takes a file as an arguement and returns a C<DateTime> object representing
+its creation date. Falls back to the creation date specified by the 
+filesystem if no plugin is available.
+
+	my $dt = Media::DateTime->datetime( $file );
+	# or
+	my $dt = $dater->datetime( $file );
+
+=back
+
+=head1 SEE ALSO
+
+See the excellent C<DateTime> module which simplifies the handling of dates.
+See C<Module::Pluggable> and C<Module::Pluggable::Ordered> which are used
+to implement the plugin system. C<Image::Info> is used to extract data from
+JPEG files for the C<Media::DateTime::JPEG> plugin.
+
+Make sure you have configured the local time zone on your machine. See
+C<DateTime::TimeZone::Local> for information on how the timezone is 
+determined.
+
+=head1 AUTHOR
+
+Mark Grimes, E<lt>mgrimes@cpan.org<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2006 by mgrimes
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
---
+++ lib/Media/DateTime/JPEG.pm
@@ -0,0 +1,112 @@
+package Media::DateTime::JPEG;
+
+use strict;
+use warnings;
+
+use Carp;
+use Image::ExifTool;
+use DateTime;
+
+our $VERSION = '0.31';
+my $exifTool;
+
+sub datetime {
+	my ($self, $f) = @_;
+
+	$exifTool = Image::ExifTool->new() unless $exifTool;
+	$exifTool->ExtractInfo( $f ) or do {
+		warn "Exiftool unable to read: $f\nFallback to file timestamp.\n";
+		return;       
+	};                  
+
+	my $datetime = $exifTool->GetValue( 'DateTimeOriginal' );
+	do {
+		warn "JPEG does not contain DateTimeOriginal exif entry ($f),\nFallback to file timestamp.\n";
+		return;
+	} unless $datetime;
+
+
+	# DateTime format = yyyy:mm:dd hh:mm:ss
+	my ($y,$m,$d,$h,$min,$s) = $datetime =~ m/
+						(\d{4})  :	# year
+						(\d{2})  :  # month
+						(\d{2})     # day
+							\s		# space
+						(\d{2})  :  # hour 
+						(\d{2})  :  # min
+						(\d{2})     # sec
+					/x
+		or die "failed DateTime pattern match in $f\n";
+
+	my $date = DateTime->new(	year	=> $y,
+								month	=> $m,
+								day		=> $d,
+		   						hour	=> $h,
+								minute	=> $min,
+								second	=> $s,
+					) or die "couldnt create DateTime";
+
+	return $date;
+}
+
+sub match { 
+	my ($self,$f) = @_;
+
+	return $f =~ /\.jpe?g$/i;			## no critic
+	# TODO: should we use something more complicated here? maybe mime type?
+}   
+
+1;
+
+__END__
+
+=head1 NAME
+
+Media::DateTime::JPEG - A plugin for the C<Media::DateTime> module to support
+JPEG files.
+
+=head1 SYNOPSIS
+
+C<Media::DateTime::JPEG> shouldn't be used directly. See C<Media::DateTime>.
+
+=head1 METHODs
+
+=over 2
+
+=item match
+
+Takes a filename as an arguement. Used by the plugin system to determine if
+this plugin should be utilized for the file. Returns true if the filename
+ends in .jpeg or .jpg. 
+
+=item datetime
+
+Takes a filename as an arguement and returns the creation date or a false
+value if we are unable to parse it.
+
+=back
+
+=head1 SEE ALSO
+
+See C<Media::DateTime> for usage. C<Image::Info> is used to extract data from
+JPEG files.
+
+=head1 AUTHOR
+
+Mark Grimes, E<lt>mgrimes@cpan.org<gt>
+
+=head1 FUTURE PLANS
+
+May use a more flexible approach to assertaining if a file is a jpeg and 
+might check that exif data exists in the C<match> method.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2006 by mgrimes
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
